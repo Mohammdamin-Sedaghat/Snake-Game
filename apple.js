7c74d0905fcffca797f4a361c7c1eead68de8932
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-12-17 21:56:02 -0500

    implemented user interface

--- apple.js
+++ apple.js
@@ -9,7 +9,7 @@ function uploadBackground() {
     ctx.fillRect(0,0,500, 500);
 }
 
-let particleArray = [{
+let snakeArr = [{
     x: 48,
     y: 48,
 }, {
@@ -19,23 +19,99 @@ let particleArray = [{
     x: 96,
     y: 96, 
 }
-]
+];
+
+let speed = 2;
+
+let direction = 'y';
+
+
+document.addEventListener('keydown', (event)=>{
+    if (event.key == 'd') {
+        if (direction != 'x') {
+            snakeArr[snakeArr.length - 1].y = Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16;
+            snakeArr.push({
+                x: (snakeArr[snakeArr.length - 1].x) + 16,
+                y: Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16,
+            });
+            direction = 'x';
+            speed = Math.abs(speed);
+        }
+    } else if (event.key == 's') {
+        if (direction === 'x') {
+            snakeArr[snakeArr.length - 1].x = Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16;
+            snakeArr.push({
+                y: (snakeArr[snakeArr.length - 1].y) + 16,
+                x: Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16,
+            });
+            direction = 'y';
+            speed = Math.abs(speed);
+        }
+    } else if (event.key == 'a') {
+        if (direction === 'y') {
+            snakeArr[snakeArr.length - 1].y = Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16;
+            snakeArr.push({
+                x: (snakeArr[snakeArr.length - 1].x) - 16,
+                y: Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16,
+            });
+            direction = 'x';
+            speed = -Math.abs(speed);
+            console.log(speed)
+        }
+    } else if (event.key == 'w') {
+        if (direction === 'x') {
+            snakeArr[snakeArr.length - 1].x = Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16;
+            snakeArr.push({
+                y: (snakeArr[snakeArr.length - 1].y) - 16,
+                x: Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16,
+            });
+            direction = 'y';
+            speed = -Math.abs(speed);
+        }
+    }
+})
 
 function game() {
     uploadBackground();
-    particleArray.forEach((particle, i) =>{
-        if (i === 2) {
+    snakeArr.forEach((particle, i) =>{
+        if (i === snakeArr.length - 1) {
             return;
         }
         ctx.beginPath();
         ctx.fillStyle = "green";
         ctx.fillRect(particle.x, particle.y, 
-            particleArray[i+1].x - particle.x+16, 
-            particleArray[i+1].y - particle.y+16);
+            snakeArr[i+1].x - particle.x+16, 
+            snakeArr[i+1].y - particle.y+16);
         ctx.fill();
     });
 
-    // window.requestAnimationFrame(game);
-}
+    //moving the tail
+    if (snakeArr[0].x == snakeArr[1].x) {
+        if (snakeArr[0].y < snakeArr[1].y) {
+            snakeArr[0].y += Math.abs(speed)
+        } else {
+            snakeArr[0].y -= Math.abs(speed)
+        }
+    } else if (snakeArr[0].y == snakeArr[1].y){
+        if (snakeArr[0].x < snakeArr[1].x) {
+            snakeArr[0].x += Math.abs(speed)
+        } else {
+            snakeArr[0].x -= Math.abs(speed)
+        }
+    }
 
+    if (direction === 'x') {
+        snakeArr[snakeArr.length-1].x += speed;
+    } else {
+        snakeArr[snakeArr.length-1].y += speed;
+    }
+
+    if (snakeArr[0].x == snakeArr[1].x &&
+        snakeArr[0].y == snakeArr[1].y 
+    ) {
+        snakeArr.splice(0,1);
+    }
+
+    window.requestAnimationFrame(game);
+}
 img.addEventListener('load', game);
