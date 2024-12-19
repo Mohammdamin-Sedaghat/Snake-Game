10dfe4a7b64da3a937e3afb826b75652b6ecb82e
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-12-18 23:04:09 -0500

    fixed stuff

--- apple.js
+++ apple.js
@@ -34,24 +34,25 @@ let appleLoc = {
 }
 
 let direction = '+x';
+let newDir = undefined;
 
 //adding user intervension
 document.addEventListener('keydown', (event)=>{
     if (event.key.toLocaleLowerCase() == 'd') {
         if (direction[1] === 'y') {
-            direction = '+x'
+            newDir = '+x'
         }
     } else if (event.key.toLocaleLowerCase() == 's') {
         if (direction[1] === 'x') {
-            direction = '+y'
+            newDir = '+y'
         }
     } else if (event.key.toLocaleLowerCase() == 'a') {
         if (direction[1] === 'y') {
-            direction = '-x'
+            newDir = '-x'
         }
     } else if (event.key.toLocaleLowerCase() == 'w') {
         if (direction[1] === 'x') {
-            direction = '-y'
+            newDir = '-y'
         }
     } else if (event.key == ' ') {
         console.log('rawr')
@@ -60,14 +61,7 @@ document.addEventListener('keydown', (event)=>{
 });
 
 function game() {
-    //checking bounderies
-    if (snakeArr[snakeArr.length - 1].x > 481 || 
-        snakeArr[snakeArr.length - 1].x < 0 ||
-        snakeArr[snakeArr.length - 1].y > 481 ||
-        snakeArr[snakeArr.length - 1].y < 0) {
-        return ;
-    }
-
+    direction = newDir || direction
     //checking if the snake has crashed
     let touched = false;
     let last = snakeArr[snakeArr.length - 1];
@@ -85,6 +79,7 @@ function game() {
         return ;
     }
 
+    //checking if the snake ate apples
     if (snakeArr[snakeArr.length - 1].x === appleLoc.x &&
         snakeArr[snakeArr.length - 1].y === appleLoc.y) {
         snakeArr.push({
@@ -94,8 +89,7 @@ function game() {
         appleLoc.x = Math.floor(Math.random() * 30) * 16;
         appleLoc.y = Math.floor(Math.random() * 30) * 16;
     } 
-    //making the background 
-    uploadBackground();
+
     //moving the snake
     snakeArr.forEach((particle, i) =>{
         if (i == snakeArr.length - 1) {
@@ -110,16 +104,29 @@ function game() {
         particle.y = snakeArr[i+1].y;
     });
 
+    //checking bounderies
+    if (snakeArr[snakeArr.length - 1].x > 480 - 16 || 
+        snakeArr[snakeArr.length - 1].x < 0 ||
+        snakeArr[snakeArr.length - 1].y > 480 - 16 ||
+        snakeArr[snakeArr.length - 1].y < 0) {
+        return ;
+    }
+
+    //making the background 
+    uploadBackground();
+
     //apples
     ctx.drawImage(apple, appleLoc.x, appleLoc.y);
     
 
     //drawing the snake
-    snakeArr.forEach((particle) =>{
-        ctx.beginPath();
+    snakeArr.forEach((particle, i) =>{
         ctx.fillStyle = "green";
+        if (i === snakeArr.length - 1) {
+            ctx.fillStyle = 'lightgreen';
+        }
         ctx.fillRect(particle.x, particle.y, 16, 16);
-        ctx.fill();
+        ctx.strokeRect(particle.x, particle.y, 16, 16);
     });
 
     move = setTimeout(game, speed);
