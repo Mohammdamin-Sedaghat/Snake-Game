481cffdd85e0074f981671dc4e676446cde4ad7d
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2024-12-18 19:13:36 -0500

    implemented timout

--- apple.js
+++ apple.js
@@ -1,6 +1,7 @@
 const ctx = document.querySelector('canvas').getContext('2d');
 const img = new Image();
 img.src = './boxes.svg';
+let move;
 
 function uploadBackground() {
     ctx.clearRect(0,0,500,500)
@@ -13,105 +14,71 @@ let snakeArr = [{
     x: 48,
     y: 48,
 }, {
-    x: 96,
+    x: 64,
     y: 48,
 }, {
+    x: 80,
+    y: 48, 
+}, {
     x: 96,
-    y: 96, 
+    y: 48, 
 }
 ];
 
-let speed = 2;
+let speed = 100;
 
-let direction = 'y';
-
-
+let direction = '+x';
+//adding user intervension
 document.addEventListener('keydown', (event)=>{
     if (event.key == 'd') {
-        if (direction != 'x') {
-            snakeArr[snakeArr.length - 1].y = Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16;
-            snakeArr.push({
-                x: (snakeArr[snakeArr.length - 1].x) + 16,
-                y: Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16,
-            });
-            direction = 'x';
-            speed = Math.abs(speed);
+        if (direction[1] === 'y') {
+            direction = '+x'
         }
     } else if (event.key == 's') {
-        if (direction === 'x') {
-            snakeArr[snakeArr.length - 1].x = Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16;
-            snakeArr.push({
-                y: (snakeArr[snakeArr.length - 1].y) + 16,
-                x: Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16,
-            });
-            direction = 'y';
-            speed = Math.abs(speed);
+        if (direction[1] === 'x') {
+            direction = '+y'
         }
     } else if (event.key == 'a') {
-        if (direction === 'y') {
-            snakeArr[snakeArr.length - 1].y = Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16;
-            snakeArr.push({
-                x: (snakeArr[snakeArr.length - 1].x) - 16,
-                y: Math.ceil(snakeArr[snakeArr.length - 1].y / 16) * 16,
-            });
-            direction = 'x';
-            speed = -Math.abs(speed);
-            console.log(speed)
+        if (direction[1] === 'y') {
+            direction = '-x'
         }
     } else if (event.key == 'w') {
-        if (direction === 'x') {
-            snakeArr[snakeArr.length - 1].x = Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16;
-            snakeArr.push({
-                y: (snakeArr[snakeArr.length - 1].y) - 16,
-                x: Math.ceil(snakeArr[snakeArr.length - 1].x / 16) * 16,
-            });
-            direction = 'y';
-            speed = -Math.abs(speed);
+        if (direction[1] === 'x') {
+            direction = '-y'
         }
+    } else if (event.key == ' ') {
+        console.log('rawr')
+        clearTimeout(move)
     }
-})
+});
 
 function game() {
+    const t = new Date;
+    //making the background 
     uploadBackground();
+    //moving the snake
     snakeArr.forEach((particle, i) =>{
-        if (i === snakeArr.length - 1) {
-            return;
+        if (i == snakeArr.length - 1) {
+            if (direction[1] == "x"){
+                particle.x = eval(particle.x + direction[0] + 16);
+            } else {
+                particle.y = eval(particle.y + direction[0] + 16);
+            }
+            return ;
         }
+        particle.x = snakeArr[i+1].x;
+        particle.y = snakeArr[i+1].y;
+    });
+    
+
+    //drawing the snake
+    snakeArr.forEach((particle) =>{
         ctx.beginPath();
         ctx.fillStyle = "green";
-        ctx.fillRect(particle.x, particle.y, 
-            snakeArr[i+1].x - particle.x+16, 
-            snakeArr[i+1].y - particle.y+16);
+        ctx.fillRect(particle.x, particle.y, 16, 16);
         ctx.fill();
     });
 
-    //moving the tail
-    if (snakeArr[0].x == snakeArr[1].x) {
-        if (snakeArr[0].y < snakeArr[1].y) {
-            snakeArr[0].y += Math.abs(speed)
-        } else {
-            snakeArr[0].y -= Math.abs(speed)
-        }
-    } else if (snakeArr[0].y == snakeArr[1].y){
-        if (snakeArr[0].x < snakeArr[1].x) {
-            snakeArr[0].x += Math.abs(speed)
-        } else {
-            snakeArr[0].x -= Math.abs(speed)
-        }
-    }
-
-    if (direction === 'x') {
-        snakeArr[snakeArr.length-1].x += speed;
-    } else {
-        snakeArr[snakeArr.length-1].y += speed;
-    }
-
-    if (snakeArr[0].x == snakeArr[1].x &&
-        snakeArr[0].y == snakeArr[1].y 
-    ) {
-        snakeArr.splice(0,1);
-    }
-
-    window.requestAnimationFrame(game);
+    move = setTimeout(game, speed);
 }
 img.addEventListener('load', game);
